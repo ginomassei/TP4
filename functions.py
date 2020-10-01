0a6f0b42a18cc5736ddd733f12e36d4078feb218
Gino Massei <ginomassei@Ginos-MacBook-Pro.local>
2020-10-01 16:38:49 -0300

    Class Pais added

---
+++ functions.py
@@ -0,0 +1,74 @@
+# ANSI colours functions.
+def print_red_text(string):
+    red_text = '\033[31;1m'
+    reset_text = '\033[m'
+
+    print(red_text + string + reset_text)
+
+
+def print_green_text(string):
+    green_text = '\033[32;1m'
+    reset_text = '\033[m'
+
+    print(green_text + string + reset_text)
+
+
+def print_blue_text(string):
+    blue_text = '\033[34;1m'
+    reset_text = '\033[m'
+
+    print(blue_text + string + reset_text)
+
+
+def striketrought_print(string):
+    crossed_text = '\033[09;37;37m'
+    reset_text = '\033[m'
+
+    print(crossed_text + string + reset_text)
+
+
+# Class definition.
+class Pais:
+    def __init__(self, confederation, name, points, wins):
+        self.confederation = confederation
+        self.name = name
+        self.points = points
+        self.wins = wins
+
+    def __str__(self):
+        # Defining confederation name based in the code.
+        conf = ''
+        if self.confederation == 0:
+            conf = 'UEFA'
+        elif self.confederation == 1:
+            conf = 'CONCACAF'
+        elif self.confederation == 2:
+            conf = 'CAF'
+        elif self.confederation == 4:
+            conf = 'AFC'
+        elif self.confederation == 5:
+            conf = 'OFC'
+
+        s = ''  # Formating the output sring.
+        s += '{:<8}'.format('Confederación: ' + str(conf))
+        s += '{:<15}'.format('Nombre: ' + str(self.name))
+        s += '{:<5}'.format('Puntos: ' + str(self.points))
+        s += '{:<3}'.format('Cantidad de campeonatos ganados: ' + str(self.wins))
+        return s
+
+
+# TP Functions.
+def load_text_file(path):
+    file = open(path, 'rt')
+    pass
+
+
+def add_in_order(p, paciente): 
+    n = len(p)
+    pos = n
+    for i in range(n):
+        if paciente.hist_clinica < p[i].hist_clinica: 
+            pos = i
+            break
+
+    p[pos:pos] = [paciente] 
--- main.py
+++ main.py
@@ -1,39 +1,14 @@
-def print_red_text(string):
-    red_text = '\033[31;1m'
-    reset_text = '\033[m'
-
-    print(red_text + string + reset_text)
-
-
-def print_green_text(string):
-    green_text = '\033[32;1m'
-    reset_text = '\033[m'
-
-    print(green_text + string + reset_text)
-
-
-def print_blue_text(string):
-    blue_text = '\033[34;1m'
-    reset_text = '\033[m'
-
-    print(blue_text + string + reset_text)
-
-
-def striketrought_print(string):
-    crossed_text = '\033[09;37;37m'
-    reset_text = '\033[m'
-
-    print(crossed_text + string + reset_text)
+import functions as f
 
 
 def main():
-    print_blue_text('TP4 Sistema de Gestión de una Competencia Mundial.\n')
+    f.print_blue_text('TP4 Sistema de Gestión de una Competencia Mundial.\n')
     print('-' * 80)
     
 
     option = -1
     while option != 0:
-        print_blue_text('Opciones: ')
+        f.print_blue_text('Opciones: ')
         print('\n1 - Mostrar el listado completo de países.')
         print('2 - Mostrár país con mayor cantidad de campeonatos ganados.')
         print('3 - Mostrar los países que ganaron algún campeonato.')
@@ -42,6 +17,9 @@ def main():
         print('0 - Salir.')
 
         option = int(input('\nIngrese su opción: '))
+
+        if option == 0:
+            f.print_blue_text('\nPrograma finalizado...')
 
         if option == 1:
             pass
