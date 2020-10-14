d52b6792b9354ab95e0169373b7b546befd5f502
Gino Massei <ginomassei@Ginos-MacBook-Pro.local>
2020-10-14 08:45:49 -0300

    Optimized __str__

--- country.py
+++ country.py
@@ -8,23 +8,11 @@ class Country:
 
     def __str__(self):
         # Decodificación de la confederación en base a su número de clave.
-        confederation_as_string = ''
-        if self.confederation == 0:
-            confederation_as_string = 'UEFA'
-        elif self.confederation == 1:
-            confederation_as_string = 'CONMEBOL'
-        elif self.confederation == 2:
-            confederation_as_string = 'CONCACAF'
-        elif self.confederation == 3:
-            confederation_as_string = 'CAF'
-        elif self.confederation == 4:
-            confederation_as_string = 'AFC'
-        elif self.confederation == 5:
-            confederation_as_string = 'OFC'
-
+        as_string = ('UEFA', 'CONMEBOL', 'CONCACAF', 'CAF', 'AFC', 'OFC')
+        
         # Formateo del string de salida para mostrar por terminal.
         s = "Confederación: {:<10} | Nombre: {:<30} | Puntos: {:<5} | Cantidad de campeonatos ganados: {:<5}"
-        return s.format(confederation_as_string, self.name, self.points, self.wins)
+        return s.format(as_string[self.confederation], self.name, self.points, self.wins)
 
 
 class CountryWithoutConfederation:
